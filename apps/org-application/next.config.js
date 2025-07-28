2c265a64796886559941f35680d24cd75e2fb3a8
Ashwini Patil <ashwinipatil100313@gmail.com>
2025-07-28 11:12:46 +0530

    done

--- apps/org-application/next.config.js
+++ apps/org-application/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -9,7 +10,28 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  nx: {
+    svgr: false,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@org-workspace/ai-service': path.resolve(
+        __dirname,
+        '../../libs/ai-service/src'
+      ),
+      '@org-workspace/vector-service': path.resolve(
+        __dirname,
+        '../../libs/vector-service/src'
+      ),
+      '@org-workspace/database': path.resolve(
+        __dirname,
+        '../../libs/database/src'
+      ),
+    };
+
+    return config;
+  },
 };
 
 const plugins = [
